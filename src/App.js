e84bd37980b4b21d8f55e04f9ae7274c754e6122
TelamonPegasus <telamon025115@gmail.com>
2020-09-07 14:56:10 +0300

    Task eklemek için input ve button eklendi.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            items: [],
+            currentItem:{
+                text:'',
+                key:''
+            }
+        }
+    }
+    render() {
+        return (
+            <div className="todoWindow">
+                <div className="orta">
+                    <div>Merhaba Burak, Hoşgeldin!</div>
+                    <div>Ne yapılması gerekiyor?</div>
+                </div>
+                <input type="text" placeholder="Enter task" value={this.state.currentItem}/>
+                <button type="input">Ekle</button>
+            </div>
+        );
+    }
 }
 
 export default App;
